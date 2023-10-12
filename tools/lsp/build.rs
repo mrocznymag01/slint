8a0f26946b15d2642b01f9a8b33d9a1ed8dca350
Florian Blasius <florian.blasius@slint-ui.com>
2023-10-12 07:52:20 +0200

    Move preview ui code to slint file.

--- tools/lsp/build.rs
+++ tools/lsp/build.rs
@@ -2,5 +2,6 @@
 // SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-1.1 OR LicenseRef-Slint-commercial
 
 fn main() {
+    slint_build::compile("preview/ui/preview-ui.slint").unwrap();
     println!("cargo:rustc-env=SLINT_ENABLE_EXPERIMENTAL_FEATURES=1");
 }
